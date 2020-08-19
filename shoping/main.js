ef76bdab83ec3a5c40132d208bd0a3280e7eb51b
areum514 <21500514@handong.edu>
2020-08-19 10:41:08 +0900

    Computed Properties

--- shoping/main.js
+++ shoping/main.js
@@ -1,23 +1,25 @@
 var app=new Vue({
     el:"#app",
     data:{
+        brand:'Vue mastery',
         product:'sock',
-        image:'./vmSocks-green.jpg',
+        selectedVariant:0,
         link: 'https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks',
         inventory:5,
         onSale:true,
-        inStock:false,
         details: ['80% cotton', '20% polyester', 'Gender-neutral'],
         variants: [
             {
                 variantId: 2234,
                 variantColor: 'green',
-                variantImage:'./vmSocks-green.jpg'
+                variantImage:'./vmSocks-green.jpg',
+                variantQuantity:0
             },
             {
                 variantId: 2235,
                 variantColor: 'blue',
-                variantImage: './vmSocks-blue.jpg'
+                variantImage: './vmSocks-blue.jpg',
+                variantQuantity: 10
             }
         ],
         sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL'],
@@ -27,13 +29,32 @@ var app=new Vue({
         addToCart:function(){
             this.cart+=1
         },
-        updateProduct:function(value){
-            this.image = value;
+        updateProduct: function (index){
+            this.selectedVariant = index;
+            
         },
         removeToCart: function () {
             this.cart -= 1
         },
       
     },
+    computed:{
+        title(){
+            return this.brand + " " + this.product
+        },
+        image(){
+            return this.variants[this.selectedVariant].variantImage
+        },
+        inStock(){
+            return this.variants[this.selectedVariant].variantQuantity
+        },
+        sale() {
+            if (this.onSale) {
+                return this.brand + ' ' + this.product + ' are on sale!'
+            }
+            return this.brand + ' ' + this.product + ' are not on sale'
+        }
+
+    }
     
 })
